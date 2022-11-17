82f153c1d3183ae3e3a4100033030d443e0bab62
unknown <raquelresende_@outlook.com>
2022-11-17 16:12:35 -0300

    Javascript adicionado

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+let nome = document.querySelector('#nome')
+let erroNome = document.querySelector('#erroNome')
+
+let email = document.querySelector('#email')
+let erroEmail =document.querySelector('#erroEmail')
+
+let assunto = document.querySelector('#assunto')
+let erroAssunto =document.querySelector('#erroAssunto')
+
+let nomeOk= false
+let emailOk= false
+let assuntoOk= false
+
+let mapa= document.querySelector('#mapa')
+
+function validaNome(){
+    if(nome.value.length < 3){
+    erroNome.innerHTML= 'Nome inválido'
+    nomeOk=false
+}else{
+    erroNome.innerHTML=''
+    nomeOk= true 
+}
+}
+ function validaEmail(){
+    if(email.value.indexOf('@')== -1 || email.value.indexOf ('.')== -1){
+     erroEmail.innerHTML="Email inválido"
+     emailOk=false
+ }else{ 
+    erroEmail.innerHTML=''
+    emailOk= true
+ }
+
+}
+
+function validaAssunto(){
+    if(assunto.value.length > 5){
+        erroAssunto.innerHTML="Limite de caracter excedido"
+        assuntoOk=false
+    }else{
+        erroAssunto.innerHTML=""
+        assuntoOk= true
+
+
+    }
+
+}
+function enviar(){
+    if(nomeOk && emailOk && assuntoOk){
+        alert('Email enviado com sucesso!')
+    }else{
+    alert ('Preencha as informações correta.')
+}
+}
+
+function mapaZoom(){
+    mapa.style.width='500px'
+    mapa.style.height='300px'
+}
+
+function mapaNormal(){
+    mapa.style.width='400px'
+    mapa.style.height='250px'
+}
